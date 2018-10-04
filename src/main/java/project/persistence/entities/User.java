0de646f4db3441868c132c502c4cb43b912850f0
unknown <hugiholm1@gmail.com>
2018-10-04 21:56:12 +0000

    signup lagad

--- src/main/java/project/persistence/entities/User.java
+++ src/main/java/project/persistence/entities/User.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
  * Be sure to annotate any entities you have with the @Entity annotation.
  */
 @Entity
-@Table(name = "user") // If you want to specify a table name, you can do so here
+@Table(name = "bet_user") // If you want to specify a table name, you can do so here, má ekki vera bara "user"
 public class User {
 
     // Declare that this attribute is the id
