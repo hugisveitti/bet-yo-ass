3fb6da114cbcf4d8d624d5f5a0971c9bbcf3a390
mombanger69 <hugiholm1@gmail.com>
2018-10-19 13:42:44 +0000

    HMMMMMMMMMMM

--- src/main/java/project/controller/PendingBetController.java
+++ src/main/java/project/controller/PendingBetController.java
@@ -1,5 +1,6 @@
 package project.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -16,6 +17,7 @@ public class PendingBetController {
 
     private PendingBetService pendingBetService;
 
+    @Autowired
     public PendingBetController(PendingBetService pendingBetService){
         this.pendingBetService = pendingBetService;
     }
--- src/main/java/project/service/Implementation/PendingBetServiceImplementation.java
+++ src/main/java/project/service/Implementation/PendingBetServiceImplementation.java
@@ -1,15 +1,19 @@
 package project.service.Implementation;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import project.persistence.entities.PendingBet;
 import project.persistence.repositories.PendingBetRepository;
 import project.service.PendingBetService;
 
 import java.util.List;
 
+@Service
 public class PendingBetServiceImplementation implements PendingBetService {
 
     PendingBetRepository repository;
 
+    @Autowired
     public PendingBetServiceImplementation(PendingBetRepository repository){
         this.repository = repository;
     }
